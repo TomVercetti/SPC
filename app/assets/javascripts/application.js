becda728ce035bbbf901e6ee277d0ef5ce58bb65
Tommy Vercetti <dailyhub8@gmail.com>
2017-07-03 10:31:44 +0100

    Changed order of javascript to fix signout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery-ui
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
